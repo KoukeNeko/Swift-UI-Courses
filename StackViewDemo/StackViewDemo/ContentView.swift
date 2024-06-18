4dacf468cccf015c8419679ad0736458d1974c68
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2024-06-18 22:54:43 +0800

    Using Images

---
+++ StackViewDemo/StackViewDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  StackViewDemo
+//
+//  Created by 陳德生 on 2024/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Instant Developer")
+                            .fontWeight(.medium)
+                            .font(.system(size: 40))
+                            .foregroundColor(.indigo)
+
+            Text("Get help from experts in 15 minutes")
+            
+            Image("user1")
+            
+            Spacer()
+        }
+        .padding(.top, 38)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StackViewDemo/StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by 陳德生 on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
