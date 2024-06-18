0db6b4b7fe6c0c2c679e9363efb13d760f521ad5
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2024-06-18 22:59:07 +0800

    Layout the Buttons Using a Stack View

--- StackViewDemo/StackViewDemo/ContentView.swift
+++ StackViewDemo/StackViewDemo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("Instant Developer")
                             .fontWeight(.medium)
                             .font(.system(size: 40))
@@ -17,12 +17,37 @@ struct ContentView: View {
 
             Text("Get help from experts in 15 minutes")
             
-            Image("user1")
-                .resizable()
-//                .scaledToFit()
-                .aspectRatio(contentMode: .fit)
+            HStack(
+                alignment: .bottom, spacing: 10
+            ) {
+                Image("user1")
+                    .resizable()
+                    .scaledToFit()
+
+                Image("user2")
+                    .resizable()
+                    .scaledToFit()
+
+                Image("user3")
+                    .resizable()
+                    .scaledToFit()
+            }.padding(.horizontal, 20)
+            
+            Text("Need help with coding problems? Register!")
             
             Spacer()
+            
+            Button {
+
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
         }
         .padding(.top, 38)
     }
